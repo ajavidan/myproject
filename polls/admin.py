2d9fd57861b7f0ce56e621b2ab1ed705dbe77f0c
Blair Javidan <armaghan.javidan@gmail.com>
2015-11-16 16:47:32 -0800

    views and urls

--- polls/admin.py
+++ polls/admin.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets=[
-    ('Question Information', {'fields' : ['question_text']}),
-    ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+        (None , {'fields' : ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date', 'question_text']
+    search_fields = ['question_text', 'pub_date']
 
 admin.site.register(Question, QuestionAdmin)
-admin .site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -8,8 +8,13 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+        
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published Recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
@@ -17,5 +22,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-
-        
