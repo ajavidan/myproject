fe83e833acb7bcd7ef9eb04a02ae9e879f5a9757
Blair Javidan <armaghan.javidan@gmail.com>
2015-11-16 17:44:59 -0800

    added tests for was_published_recently function in Question

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from .models import Question
+import datetime
 
-# Create your tests here.
+class QuestionMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=30)
+        past_quesiton = Question(pub_date=time)
+        self.assertEqual(past_quesiton.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
+
+    def create_question(question_text, days):
+        time=timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=question_text, pub_date=time)
