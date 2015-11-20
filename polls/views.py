b98e362f9e4b73dccdc00f73cae99cdb094da735
Blair Javidan <armaghan.javidan@gmail.com>
2015-11-20 09:42:08 -0800

    "fixed directory structure"

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -16,7 +17,7 @@ class IndexView(generic.ListView):
     context_object_name= 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 #def detail(request, question_id):
 #    question = get_object_or_404(Question, pk=question_id)
@@ -30,6 +31,9 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 #def results(request, question_id):
 #    response = ("You are looking at results of question %s.")
